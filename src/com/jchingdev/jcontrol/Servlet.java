23da35cef549ddce4df3ee666f210d76bf3f6970
JoshuaChing <joshua-ching@hotmail.com>
2014-03-29 02:18:47 -0400

    Number of Texts Field

--- src/com/jchingdev/jcontrol/Servlet.java
+++ src/com/jchingdev/jcontrol/Servlet.java
@@ -19,37 +19,53 @@ public class Servlet extends HttpServlet{
 	private static final long serialVersionUID = 6018370847996819359L;
 
 	//method called when user enters URL
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		resp.setContentType("text/html");
 		resp.setStatus(HttpServletResponse.SC_OK);
 		PrintWriter out=resp.getWriter();
-		out.println("<html style='background-color:#2c3e50'>");
-		out.println("<head>");
-		out.println("<title>jControl</title");
-		out.println("</head>");
-		out.println("<body style='color:#FFFFFF;font-family:Verdana'>");
-		out.println("");
-		out.println("<center>");
-		out.println("<h1>Welcome to jControl</h1>");
-		out.println("Last updated: "+ (new java.util.Date())+"<br><br>");
-		out.println("<h3>Hardware Information</h3");
-		out.println("ID: "+Build.ID+"<br>");
-		out.println("Model Number: "+Build.MODEL+"<br>");
-		out.println("Android SDK: "+Build.VERSION.SDK+"<br><br>");
-		out.println("<h3>Send a text message</h3>");
-		out.println("<form method='post'>");
-		out.println("Phone #: ");
-		out.println("<input type='text' name='phoneNumber'/> <br>");
-		out.println("Message: ");
-		out.println("<input type='text' name='message'/> <br>");
-		out.println("<input type='submit' value='send'/>");
-		out.println("</form>");
-		out.println("</center>");
-		out.println("</body>");
-		out.println("</html>");
-	    }
+		out.println
+		(
+				"<html style='background-color:#2c3e50'>" +
+				"<head>" +
+				"	<title>jControl</title>" +
+				"	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>" +
+				"	<script>" +
+				"		$(document).ready(function(){" +
+				"			$('#updateTime').click(function(){" +
+				"				$.post('http://192.168.0.17:8080/', null, function(newTime){" + //need to get proper IP
+				"					$('time').text(newTime);" +
+				"					alert('clicked');" +
+				"				});" +
+				"			});" +
+				"		});" +
+				"	</script>" +
+				"</head>" +
+				"<body style='color:#FFFFFF;font-family:Verdana'>" +
+				"	<center>" +
+				"		<h1>Welcome to jControl</h1>" +
+				"		<p>" +
+				"			Last updated: <div id='time'>" + (new java.util.Date()) + "</div>" +
+				"			<button id='updateTime'>Update Time</button>" +
+				"		</p>" +
+				"		<h3>Hardware Information</h3>" +
+				"		<p>" +
+				"			ID: "+Build.ID+"<br>" +
+				"			Model Number: "+Build.MODEL+"<br>" +
+				"			Android SDK: "+Build.VERSION.SDK_INT+"<br>" +
+				"		</p>" +
+				"		<h3>Send a text message</h3>" +
+				"		<form method='post'>" +
+				"			Phone #: <input type='text' name='phoneNumber'/> <br>" +
+				"			Message: <input type='text' name='message'/> <br>" +
+				"			# of Texts: <input type='number' name='numberOfTexts' min='1' max='20' value='1'> <br>" +
+				"			<input type='submit' name ='send' value='send'/>" +
+				"		</form>" +
+				"	</center>" +
+				"</body>" +
+			"</html>"		
+		);
+	}	
 	
 	//method for post
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
@@ -57,20 +73,27 @@ public class Servlet extends HttpServlet{
 		resp.setStatus(HttpServletResponse.SC_OK);
 		PrintWriter out=resp.getWriter();
 		
-		String phoneNumber=req.getParameter("phoneNumber");
-		String message=req.getParameter("message");
-		 if(phoneNumber==null || message==null || phoneNumber=="" ||message=="") {
-	        out.println("<h1>Invalid input</h1>");
-	        return;
-		 }else{
-			 sendSMS(phoneNumber, message);
-			 out.println("<h1>\""+message+"\"sent to "+phoneNumber);
-		 }
+		if (req.getParameter("send") != null){
+			String phoneNumber=req.getParameter("phoneNumber");
+			String message=req.getParameter("message");
+			String numberOfTexts= req.getParameter("numberOfTexts");
+			if(phoneNumber==null || message==null || phoneNumber=="" ||message=="" || numberOfTexts==null || numberOfTexts=="") {
+				out.println("<h1>Invalid input</h1>");
+			}else{
+				sendSMS(phoneNumber, message, Integer.parseInt(numberOfTexts));
+				out.println("<h1>\""+message+"\"sent to "+phoneNumber+" ");
+			}	
+		}
+		else if (req.getParameter("updateTime") != null){
+			out.println(new java.util.Date());
+		}
 	}
 	
 	//method for sending sms
-	private void sendSMS(String phoneNumber, String message){
+	private void sendSMS(String phoneNumber, String message, int number){
 		SmsManager sms=SmsManager.getDefault();
-		sms.sendTextMessage(phoneNumber, null, message, null, null);
+		for (int i=0; i < number; i++){
+			sms.sendTextMessage(phoneNumber, null,message, null, null);
+		}
 	}
 }
