188a677bacfef8af6e81e19dbdec7cbf2376a741
JoshuaChing <joshua-ching@hotmail.com>
2014-04-04 19:15:40 -0400

    cleaned up code

--- gen/com/jchingdev/jcontrol/R.java
+++ gen/com/jchingdev/jcontrol/R.java
@@ -29,8 +29,9 @@ public final class R {
     }
     public static final class id {
         public static final int IP=0x7f090001;
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f090003;
         public static final int instructions=0x7f090000;
+        public static final int title=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,6 +44,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int instructions=0x7f050003;
+        public static final int title=0x7f050004;
     }
     public static final class style {
         /** 
---
+++ src/com/jchingdev/quicktext/HttpServer.java
@@ -0,0 +1,85 @@
+package com.jchingdev.quicktext;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.text.format.Formatter;
+import android.util.Log;
+
+public class HttpServer {
+	
+	private ServletContextHandler handler;
+	private Server server;
+	private Context context;
+	private int port=8080;
+	
+	//for Android 2.x bug
+	static{
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB){
+			System.setProperty("java.net.preferIPv6Addresses", "false");
+		}
+	}
+	
+	//jetty server and listen at port 8080
+	HttpServer(Context context){
+		this.context=context;
+		
+		server = new Server(port); //port
+		handler = new ServletContextHandler();
+		handler.setContextPath("/");
+		handler.addServlet(new ServletHolder(new Servlet()), "/");
+		server.setHandler(handler);
+	}
+	
+	//run server in separate thread
+	public void start(){
+		new Thread(){
+			public void run(){
+				try{
+					server.start();
+				}catch(Exception e){
+					Log.e("SERVER_START",e.toString());
+				}
+			}
+		}.start();
+	}
+	
+	//stop server
+	public void stop(){
+		try{
+			server.stop();
+		}catch(Exception e){
+			Log.e("SERVER_STOP",e.toString());
+		}
+	}
+	
+	//get ip address of device
+	@SuppressWarnings("deprecation")
+	public String getAddress(){
+		ConnectivityManager con;
+		con=(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		if(con.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()){
+			//check if device is connected to wifi network
+			WifiManager wifi;
+			wifi=(WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+			
+			int IP=wifi.getConnectionInfo().getIpAddress();
+			
+			return "http://"+Formatter.formatIpAddress(IP)+":"+port;
+		}
+		else{
+			return "You are not connected to WIFI";
+		}
+	}
+	
+	//get port
+	public String getPort(){
+		return String.valueOf(port);
+	}
+}
